4947e2a15dde55176f1147a09089047e22edf30f
Amyoli03 <louwamyoli03@gmail.com>
2026-08-15 13:01:13 +0200

    Loigin changes

--- ChatAppPart1/src/main/java/com/mycompany/chatapppart1/Login.java
+++ ChatAppPart1/src/main/java/com/mycompany/chatapppart1/Login.java
@@ -10,4 +10,74 @@ package com.mycompany.chatapppart1;
  */
 public class Login {
     
+  //Declare variables that are going to be used by the user  
+    
+  String username;  
+  String password;
+  String phoneNumber;
+          
+ // user name validation
+ public boolean checkUserName(String username){
+     return username.contains("_")&&username.length()<=5;
+ }
+ 
+//Password Validation
+public boolean checkPasswordComplexity(String password){
+    
+    boolean hasCapital = false;
+    boolean hasNumber = false;
+    boolean hasSpecial = false;
+    
+    for (int i = 0; i < password.length(); i++){
+        char c = password.charAt(i);
+        
+        if (Character.isUpperCase(c)){
+            hasCapital = true;
+        } else if (Character.isDigit(c)){ 
+            hasNumber = true;
+        } else if (!Character.isLetterOrDigit(c)){
+            hasSpecial = true;
+        }
+    }
+  return password.length() >= 8 && hasCapital && hasNumber && hasSpecial;
+ }
+
+//cellphone number balidation
+public boolean checkCellPhoneNumber(String phone) {
+    return phone.startsWith("+27") && phone.length() <= 12;
+    
 }
+
+//register validation
+public String registerUser(String username, String password,String phoneNumber){
+    
+    if(!checkUserName(username)){ 
+        return "Username is not correctly formatted; please ensure that your username contains and underscore and is no more than five characters in length.";
+    }
+    
+    if(!checkPasswordComplexity(password)){
+        return "Cell phone number incorrectly formatted or does not contain international code";
+    }
+    
+    this.username= username;
+    this.password= password;
+    this.phoneNumber= phoneNumber;
+    
+    return "Username registered successfully.";
+    }
+
+//Login Feature
+public boolean loginUser(String username, String password){
+    return this.username.equals(username) && this.password.equals(password);
+}
+//Return Login feature
+public String returnLoginStatus(boolean success){
+    if (success){
+        return "Welcome" + username + "it is great to see you again.";
+    } else {
+        return "Username or password incorrect, please try again.";
+    }
+  }
+}
+
+
